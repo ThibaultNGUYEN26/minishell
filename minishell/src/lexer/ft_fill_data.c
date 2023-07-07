5528154e8bc150b5d8d91a0946c895deb4ee90e5
ThibaultNGUYEN26 <thibault.nguyen26@gmail.com>
2023-07-07 17:19:42 +0200

    fill_data modified

--- minishell/src/lexer/ft_fill_data.c
+++ minishell/src/lexer/ft_fill_data.c
@@ -6,7 +6,7 @@
 /*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 21:03:08 by thibnguy          #+#    #+#             */
-/*   Updated: 2023/07/04 18:23:06 by thibnguy         ###   ########.fr       */
+/*   Updated: 2023/07/06 15:23:02 by thibnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,99 +17,123 @@
   * @param input
   * @returns data
   */
-t_data	*ft_fill_data(char *input, char **envp)
+/* t_data   *ft_fill_data(char *input, char **envp)
 {
-	t_data	*data;
-	char	**tab;
-	int		i;
+    t_data  *data;
+    char    **tab;
+    int     i;
 
-	(void)envp;
-	tab = NULL;
-	data = NULL;
-	tab = ft_split(input, " ");
-	i = -1;
-	while (tab[++i])
-		addlast_node(&data, ft_new_stack(tab[i]));
-	free(tab);
-	return (data);
-}
-
-/* static char	*ft_env_content(char *input, char **envp)
-{
-	char	*element;
-	char	*var;
-	int		i;
-
-	i = 0;
-	while (input[i] != ' ' && input[i])
-		i++;
-	var = ft_substr(input, 0, i);
-	while (*envp && ft_strncmp(*envp, var, ft_strlen(var)))
-		envp++;
-	if (*envp)
-		element = *envp + ft_strlen(var) + 1;
-	else
-		element = "\0";
-	return (element);
-}
-
-t_data	*ft_fill_data(char *input, char **envp)
-{
-	t_data	*data;
-	char	c;
-	int		i;
-	int		j;
-	
-	i = 0;
-	data = NULL;
-	while (i < ft_strlen(input))
-	{
-		j = i;
-		if (input[i] == '<' || input[i] == '>' || input[i] == '|')
-			addlast_node(&data, ft_new_stack(ft_substr(input, i, 1)));
-		else if (!ft_strncmp(input + i, ">>", 2) || !ft_strncmp(input + i, "<<", 2))
-			addlast_node(&data, ft_new_stack(ft_substr(input, i, 2)));
-		else if (input[i] == '\"' || input[i] == '\'')
-		{
-			c = input[i];
-			printf("%d\n", i);
-			while (input[++j] && input[j] != c);
-			printf("%d\n", j);
-			if (!input[j])
-			{
-				printf("Error\n");
-				exit(0);
-			}
-			addlast_node(&data, ft_new_stack(ft_substr(input, i + 1, j - i - 1)));
-			i = j + 1;
-			printf("%s\n", input + i);
-		}
-		else if (input[i] == ' ')
-			i++;
-		else
-		{
-			if (input[i] == '$')
-			{
-				if (input[i + 1] == '?')
-				{
-					addlast_node(&data, ft_new_stack("$?"));
-					i += 2;
-				}
-				else
-				{
-					addlast_node(&data, ft_new_stack(ft_env_content(input + i + 1, envp)));
-					while (input[i] && input[i] != ' ')
-						i++;
-				}
-			}
-			else
-			{
-				while (input[j] && input[j] != ' ')
-					j++;
-				addlast_node(&data, ft_new_stack(ft_substr(input, i, j - i)));
-				i = j + 1;
-			}
-		}
-	}
-	return (data);
+    (void)envp;
+    tab = NULL;
+    data = NULL;
+    tab = ft_split(input, " ");
+    i = -1;
+    while (tab[++i])
+        addlast_node(&data, ft_new_stack(tab[i]));
+    free(tab);
+    return (data);
 } */
+
+static char *ft_simple_quotes(char *input, int *i)
+{
+    char    *res;
+    int     j = *i;
+    char    c;
+    
+    res = NULL;
+    c = input[*i];
+    while (input[++j] && input[j] != c);
+    if (!input[j])
+    {
+        printf("Error\n");
+        exit(0);
+    }
+    res = ft_substr(input, *i + 1, j - *i - 1);
+    *i = j + 1;
+    return (res);
+}
+
+static char *ft_env_content(char *input, char **envp)
+{
+    char    *element;
+    char    *var;
+    int     i;
+
+    i = 0;
+    while ((input[i] != ' ' || input[i] != '\'' || input[i] != '\"') && input[i])
+        i++;
+    var = ft_substr(input, 0, i);
+    while (*envp && ft_strncmp(*envp, var, ft_strlen(var)))
+        envp++;
+    if (*envp)
+        element = *envp + ft_strlen(var) + 1;
+    else
+        element = "\0";
+    return (element);
+}   
+
+static char *ft_dollar_sign(char *input, int *i, char **envp)
+{
+    char    *res;
+    
+    res = NULL;
+    if (input[*i + 1] == '?')
+    {
+        res = "$?";
+        *i += 2;
+    }
+    else if (input[*i + 1] == ' ' || input[*i + 1] == '\0')
+    {
+        res = "$";
+        *i += 1;
+    }
+    else if (input[*i + 1] == '\'' || input[*i + 1] == '\"')
+        *i += 1;
+    else
+    {
+        res = ft_env_content(input + (*i) + 1, envp);
+        while (input[*i] && input[*i] != ' ')
+            *i += 1;
+    }
+    return (res);
+}
+
+t_data  *ft_fill_data(char *input, char **envp)
+{
+    t_data  *data;
+    char    *res;
+    int     i;
+    int     j;
+    
+    i = 0;
+    data = NULL;
+    while (i < ft_strlen(input))
+    {
+        j = i;
+        if (input[i] == '<' || input[i] == '>' || input[i] == '|')
+            addlast_node(&data, ft_new_stack(ft_substr(input, i, 1)));
+        else if (!ft_strncmp(input + i, ">>", 2) || !ft_strncmp(input + i, "<<", 2))
+            addlast_node(&data, ft_new_stack(ft_substr(input, i, 2)));
+        else if (input[i] == '\'')
+            addlast_node(&data, ft_new_stack(ft_simple_quotes(input, &i)));
+        else if (input[i] == ' ')
+            i++;
+        else
+        {
+            if (input[i] != '$')
+            {
+                while (input[j] && input[j] != ' ')
+                    j++;
+                addlast_node(&data, ft_new_stack(ft_substr(input, i, j - i)));
+                i = j + 1;
+            }
+            else
+            {
+                res = ft_dollar_sign(input, &i, envp);
+                if (res)
+                    addlast_node(&data, ft_new_stack(ft_dollar_sign(input, &i, envp)));
+            }
+        }
+    }
+    return (data);
+}
---
+++ minishell/src/signals/utils/ft_stack_utils.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stack_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 09:59:21 by thibnguy          #+#    #+#             */
+/*   Updated: 2023/07/03 15:40:12 by thibnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+/**
+  * Malloc a linked list.
+  * @param content 
+  * @returns new
+  */
+t_data	*ft_new_stack(char *content)
+{
+	t_data	*new;
+
+	new = malloc(sizeof(t_data) * 1);
+	if (!new)
+		return (NULL);
+	new->next = new;
+	new->prev = new;
+	new->content = content;
+	return (new);
+}
+
+/**
+  * Add element at end of linked list.
+  * @param stack
+  * @param new_data
+  * @returns void
+  */
+void	addlast_node(t_data **stack, t_data *new_data)
+{
+	if (*stack == NULL)
+	{
+		*stack = new_data;
+		(*stack)->next = *stack;
+		(*stack)->prev = *stack;
+		return ;
+	}
+	((*stack)->prev)->next = new_data;
+	(new_data)->prev = (*stack)->prev;
+	(new_data)->next = *stack;
+	(*stack)->prev = new_data;
+}
+
+/**
+  * Free linked list.
+  * @param void 
+  * @returns data
+  */
+void	ft_free_stack(t_data *data)
+{
+	t_data	*head;
+
+	head = data;
+	if (data->next == data)
+	{
+		free(data->content);
+		free(data);
+		return ;
+	}
+	data = data->next;
+	while (data != head)
+	{
+		free(data->content);
+		data = data->next;
+		free(data->prev);
+	}
+	free(data->content);
+	free(data);
+}
+
+/**
+  * Print linked list.
+  * @param void
+  * @returns data
+  */
+void	ft_print_data(t_data *data)
+{
+	t_data	*head;
+	int		i;
+
+	head = data;
+	i = 0;
+	while (1)
+	{
+		printf("[%d] %s\n", i, (data)->content);
+		if ((data)->next == head)
+			break;
+		data = (data)->next;
+		i++;
+	}
+	data = head;
+}
+
