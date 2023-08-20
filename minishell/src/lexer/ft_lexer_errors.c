b43d22f6586a094f6b1f4d314e8592a169aef660
ThibaultNGUYEN26 <thibaultnguyen26@github.com>
2023-08-20 22:06:15 +0200

    quotes error corrected

--- minishell/src/lexer/ft_lexer_errors.c
+++ minishell/src/lexer/ft_lexer_errors.c
@@ -6,7 +6,7 @@
 /*   By: rchbouki <rchbouki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 19:06:41 by rchbouki          #+#    #+#             */
-/*   Updated: 2023/08/20 19:17:59 by rchbouki         ###   ########.fr       */
+/*   Updated: 2023/08/20 20:57:0 by rchbouki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,31 @@
 
 void	ft_quotes_error(t_data *data)
 {
-	int		i;
-	int		count_double;
-	int		count_simple;
 	t_data	*head;
+	int		i;
+	char	c;
 
 	head = data;
 	while (1)
 	{
 		i = -1;
-		count_double = 0;
-		count_simple = 0;
 		data->exit_code = 0;
 		if (data->token == 5)
 		{
 			while ((data->content)[++i])
 			{
-				if ((data->content)[i] == '\"')
-					count_double++;
-				else if ((data->content)[i] == '\'')
-					count_simple++;
+				if ((data->content)[i] == '\"' || (data->content)[i] == '\'')
+				{
+					c = (data->content)[i++];
+					while ((data->content)[i] != c && (data->content)[i])
+						i++;
+					if (!(data->content)[i])
+					{
+						data->exit_code = 2;
+						break ; 
+					}
+				}
 			}
-			if (count_double != 0 && count_double % 2 != 0)
-				data->exit_code = 2;
-			if (count_simple != 0 && count_simple % 2 != 0)
-				data->exit_code = 2;
 		}
 		if ((data)->next == head)
 			break;
